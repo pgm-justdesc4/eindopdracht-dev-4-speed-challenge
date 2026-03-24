4485150c1f57dfacbb3bf42fa9d08a99fa90f450
meesakveld <meesakveld@gmail.com>
2026-03-24 12:37:24 +0100

    Add Socket.IO server implementation and remove TypeScript configuration

---
+++ src/socket-io-server/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const { createServer } = require('http');
+const socketIO = require('socket.io');
+
+const app = express();
+const httpServer = createServer(app);
+const io = socketIO(httpServer, {
+  cors: { origin: '*' }
+});
+
+app.get('/', (req, res) => res.send('Server online'));
+
+io.on('connection', (socket) => {
+  console.log('Device connected:', socket.id);
+
+  socket.on('ping', (...args) => {
+    console.log('ping received:', JSON.stringify(args));
+    broadcastToTD({ type: 'ping', data: args[0] });
+  });
+
+});
+
+app.use((req, res, next) => {
+  console.log('HTTP:', req.method, req.url);
+  next();
+});
+
+httpServer.listen(3000, '0.0.0.0', () => console.log('Server running on port 3000'));
