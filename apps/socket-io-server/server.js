3bd86c4b8244a97d49bb382a6823eae6fd4541a2
meesakveld <meesakveld@gmail.com>
2026-03-29 17:04:14 +0200

    Updated server

--- apps/socket-io-server/server.js
+++ apps/socket-io-server/server.js
@@ -13,10 +13,19 @@ app.get('/', (req, res) => res.send('Server online'));
 io.on('connection', (socket) => {
   console.log('Device connected:', socket.id);
 
-  socket.on('button_pressed', (...args) => {
-    console.log('button pressed received:', JSON.stringify(args));
+  socket.on('button_pressed', (data) => {
+    console.log('button pressed received:', JSON.stringify(data));
+
+    // STAP 1: Stuur het bericht door naar IEDEREEN (behalve de afzender)
+    // Gebruik io.emit om het naar alle verbonden clients (zoals de Pi) te sturen
+    io.emit('button_pressed', data); 
+    
+    console.log('Broadcasted to all clients');
   });
 
+  socket.on('disconnect', () => {
+    console.log('Device disconnected:', socket.id);
+  });
 });
 
 app.use((req, res, next) => {
